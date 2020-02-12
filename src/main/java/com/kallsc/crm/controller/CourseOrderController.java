da6399d017a98c784e9c3ae9601cf02e9e0929cb
Your Name <you@example.com>
2020-02-12 17:42:44 +0800

    实现了后台页面切换功能

---
+++ src/main/java/com/kallsc/crm/controller/CourseOrderController.java
@@ -0,0 +1,13 @@
+package com.kallsc.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/courseorder")
+public class CourseOrderController {
+    @RequestMapping("/list")
+    public String list(){
+        return "courseorder/list";
+    }
+}
